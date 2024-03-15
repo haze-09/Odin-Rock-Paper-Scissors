8c738a243807c5bd959f297c06c573575ce77a53
haze-09 <68776764+haze-09@users.noreply.github.com>
2024-03-15 12:58:48 +0530

    Completed JS logic for the game

--- script.js
+++ script.js
@@ -2,9 +2,58 @@ function getComputerChoice() {
     const choices = ['rock', 'paper','scissors'];
     let randomIndex = Math.floor(Math.random() * choices.length);
     let randomChoice = choices[randomIndex];
+    console.log('Computer: '+ randomChoice);
+    
     return randomChoice;
 }
 
-let computerChoice = getComputerChoice();
-// console.log(computerChoice);
+function getUserChoice(){
+    let userChoice=prompt('choose');
+    userChoice = userChoice.toLowerCase();
 
+    if(userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors'){
+        console.log('User: '+ userChoice);
+        return userChoice;
+    }
+    else{
+        console.log('incorrect choice :(');
+        console.log('Choose between rock, paper and scissors.');
+        getUserChoice();
+    }
+    
+}
+
+let userChoice= getUserChoice();
+
+let computerChoice = getComputerChoice();
+
+if(userChoice === computerChoice){
+    console.log('Uh oh! Its a tie');
+}
+else{
+    if(userChoice === 'rock'){
+        if(computerChoice === 'scissors'){
+            console.log('You won! '+ userChoice + ' beats ' + computerChoice + '.');
+        }
+        else{
+            console.log('You lost :( ! '+ computerChoice + ' beats ' + userChoice + '.');
+        }
+    }
+    else if(userChoice === 'scissors'){
+        if(computerChoice === 'paper'){
+            console.log('You won! '+ userChoice + ' beats ' + computerChoice + '.');
+        }
+        else{
+            console.log('You lost :( ! '+ computerChoice + ' beats ' + userChoice + '.');    
+        }   
+    }
+    else if(userChoice === 'paper'){
+        if(computerChoice === 'rock'){
+            console.log('You won! '+ userChoice + ' beats ' + computerChoice + '.');
+        }
+        else{
+            console.log('You lost :( ! '+ computerChoice + ' beats ' + userChoice + '.');
+        }
+
+    }
+}
